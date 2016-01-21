249f5e85d40f16d4b5bce8965a22fc9b6fda5652
fengyao <yfeng@vwms.cn>
2016-01-21 21:42:05 +0800

    datetime filter

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # flask-sqlalchemy-datetime-example
-the usage of the datetime at flask-sqlalchemy to filter time range from start_time to end_time
+The usage of the datetime at flask-sqlalchemy to filter time range from start_time to end_time.
+This is a example about the way to get some data through time field.
+Search Time Precision: us.
+Sqlite3 Required.
+
+本例子的目的：在使用flask-sqlalchemy或sqlalchemy的情况下，
+使用datetime为每条数据增加一条插入数据的时间信息，那么就可以通过这个信息来查到特定范围内插入数据库的数据信息。
+本例子只是一个简单的例子。
+查询时间的精度可以达到微秒。
+例子使用sqlite3数据库。
+我的邮箱：yaoelvon@gmail.com
+
---
+++ time.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, './dbTest.db')
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    pub_date = db.Column(db.DateTime)
+
+    def __init__(self, username, email, pub_date):
+        self.username = username
+        self.email = email
+        self.pub_date = pub_date
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+#db.drop_all()
+db.create_all()
+
+admin = User('admin', 'admin@example.com', datetime(2015, 6, 5, 8, 10, 10, 10))
+guest1 = User('guest1', 'guest1@example.com', datetime(2015, 6, 5, 10, 20, 10, 10))
+guest2 = User('guest2', 'guest2@example.com', datetime(2015, 6, 5, 11, 12, 12, 10))
+
+db.session.add(admin)
+db.session.add(guest1)
+db.session.add(guest2)
+db.session.commit()
+
+users = User.query.all()
+print users
+
+admin = User.query.filter_by(username='admin').first()
+print admin
+
+user_range = User.query.filter(User.pub_date < datetime(2015, 6, 5, 10, 20 ,11), User.pub_date >= datetime(2015, 6, 5, 9, 11 ,11)).all()
+print user_range
